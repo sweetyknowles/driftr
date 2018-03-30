d1d8206e9df6141c9e3ee4cd0d1a33dc303d6414
mpotts0201 <mpotts0201@gmail.com>
2018-03-30 14:44:48 -0400

    Home index up and running

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
+import NavBar from './components/NavBar'
+
 
 class App extends Component {
 
   componentDidMount(){
-
+    this.getAllCities()
   }
 
   getAllCities = async() => {
-
+    const response = await axios.get('/api/cities')
+    console.log(response.data.cities)
   }
 
   render() {
     return (
       <div className="App">
-        
+        <NavBar/>
       </div>
     );
   }
