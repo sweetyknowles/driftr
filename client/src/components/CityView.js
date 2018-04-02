034de9a847443dd6e857dd8fbb230e06df9bf704
paulpellom@gmail.com <paulpellom@gmail.com>
2018-04-02 15:34:56 -0400

    styling

--- client/src/components/CityView.js
+++ client/src/components/CityView.js
@@ -1,5 +1,25 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import styled from 'styled-components'
+
+const SwagBag = styled.div`
+h1 {
+font-family: Philosopher;
+}
+h2, h4 {
+    font-family: EB Garamond;
+}
+
+#postBox {
+margin-left: 12.5vw;
+margin-bottom: 3.5vh;
+max-width: 75%;
+min-height: 200px;
+background: rgba(255, 255, 255, 0.2);
+border: 3px solid grey
+
+}
+`
 
 class CityView extends Component {
     state = {
@@ -8,13 +28,13 @@ class CityView extends Component {
     }
 
 
-    componentDidMount(){
+    componentDidMount() {
         this.getCity()
         this.getPosts()
     }
 
 
-    getPosts = async() => {
+    getPosts = async () => {
         const id = this.props.match.params.id
         const res = await axios.get(`/api/cities/${id}/posts`)
         this.setState({ posts: res.data.posts })
@@ -22,7 +42,7 @@ class CityView extends Component {
 
     }
 
-    getCity = async() => {
+    getCity = async () => {
         const id = this.props.match.params.id
         const res = await axios.get(`/api/cities/${id}`)
         this.setState({ city: res.data.city })
@@ -30,21 +50,25 @@ class CityView extends Component {
 
     render() {
         return (
-            <div>
+            <SwagBag>
                 <h1>{this.state.city.name}</h1>
-                <img src={this.state.city.image} alt={`Image of ${this.state.city.name}`}/>
-                <h2>Posts</h2>
-                {this.state.posts.map((post,i) => {
-                    return(
-                        <div>
-                            <h3>{post.title}</h3>
-                            <p>{post.text}</p>
+                <img src={this.state.city.image} alt={`Image of ${this.state.city.name}`} />
+                <h1>Posts</h1>
+                {this.state.posts.map((post, i) => {
+                    return (
+                        <div id="postBox">
+
+                            <h2>{post.title}</h2>
+                            <h4>{post.text}</h4>
                         </div>
+
                     )
                 })}
-            </div>
+            </SwagBag>
         );
     }
 }
 
 export default CityView;
+
+
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 const NavStyle = styled.div`
 .nav-wrapper {
-    background-color: seagreen;
+    background-color: rgb(0, 230, 184);
+    .brand-logo, .right { 
+        font-family: Philosopher;
+}
+li {
+    font-family: EB Garamond
+} 
 }
 `
 class NavBar extends Component {
@@ -13,11 +19,11 @@ class NavBar extends Component {
                 <nav>
 
                     <div class="nav-wrapper">
-                        <a href="#" class="brand-logo right">Driftr Lounge</a>
+                        <a href="http://localhost:3000/" class="brand-logo right">Driftr Lounge</a>
                         <ul id="nav-mobile" class="left hide-on-med-and-down">
-                            <li><a href="sass.html">Cities</a></li>
-                            <li><a href="badges.html">Posts</a></li>
-                            <li><a href="collapsible.html">About</a></li>
+                            <li><a href="/">Cities</a></li>
+                            {/* <li><a href="/">Posts</a></li> */}
+                            <li><a href="/about">About</a></li>
                         </ul>
                     </div>
                 </nav>
