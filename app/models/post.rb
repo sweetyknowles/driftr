b4777b9e1e1dd6650a52132843646576957bfe17
SweetyKnowles <sweetyjames@hotmail.co.uk>
2018-04-02 14:40:45 -0400

    validations complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :city
+ 
+  attr_accessible :title :content
+
+  # the content cannot be empty
+validates :title :content, :presence =>true
+# user is restricted to the content length between 1-200
+validates :title, :length => { :minimum => 1 }
+validates :title, :length => { :maximum => 200}
+#this will not allow the user to duplicate the post.
+validates  :title :uniqueness =>true
 end
+  
