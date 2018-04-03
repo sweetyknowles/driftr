8cd422795cde8002d24e6212d722ab5e8d33e52c
paulpellom@gmail.com <paulpellom@gmail.com>
2018-04-03 14:51:13 -0400

    advanced styling completed

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,14 @@ import axios from "axios";
 import NavBar from "./components/NavBar";
 import CityView from "./components/CityView";
 import CityList from "./components/CityList";
-import About from './components/About';
-import Intro from './components/Intro';
-import Footer from './components/Footer';
+import EditPost from "./components/EditPost";
+import About from "./components/About";
+import Intro from "./components/Intro";
+import Footer from "./components/Footer";
 
+//App.js is the centerpiece of the application. Here are the references to the components and dependencies we will be using. 
+
+// 
 
 
 class App extends Component {
@@ -23,6 +27,7 @@ class App extends Component {
             <Route exact path="/" component={CityList} />
             <Route exact path="/cities/:id" component={CityView} />
             <Route exact path="/about" component={About} />
+            <Route exact path="/cities/:id/edit" component={EditPost} />
           </Switch>
           <Footer />
         </div>
